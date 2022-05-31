b550a5f52b230c3e2a9b5532a4f4b24685ac134f
Sidney Pang <sidneypang@hotmail.co.uk>
2022-05-31 21:27:02 +0000

    finished assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function assertArrayEqual (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
+  }
+}
+
+assertArrayEqual ([1, 2, 3], [1, 2, "3"]);
+assertArrayEqual ([1, 2, 3], [1, 2, 3]);
+assertArrayEqual ([1, 2, 3], [3, 2, 3]);
