44a752d6583d96934814843b414b6caad35641af
Sidney Pang <sidneypang@hotmail.co.uk>
2022-05-31 22:05:54 +0000

    fixed typo in assertArraysEqual, added without.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -16,7 +16,7 @@ function eqArrays(array1, array2) {
   return true;
 }
 
-function assertArrayEqual (array1, array2) {
+function assertArraysEqual (array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
   } else {
@@ -24,6 +24,6 @@ function assertArrayEqual (array1, array2) {
   }
 }
 
-assertArrayEqual ([1, 2, 3], [1, 2, "3"]);
-assertArrayEqual ([1, 2, 3], [1, 2, 3]);
-assertArrayEqual ([1, 2, 3], [3, 2, 3]);
+assertArraysEqual ([1, 2, 3], [1, 2, "3"]);
+assertArraysEqual ([1, 2, 3], [1, 2, 3]);
+assertArraysEqual ([1, 2, 3], [3, 2, 3]);
---
+++ without.js
@@ -0,0 +1,38 @@
+function eqArrays(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function assertArraysEqual (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
+  }
+}
+
+function without (source, itemsToRemove) {
+  let newArray = [];
+
+  for (let item of source) {
+    if (!itemsToRemove.includes (item)) {
+      console.log (item)
+      newArray.push (item);
+    }
+  }
+  console.log (newArray);
+  return newArray;
+}
+
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
