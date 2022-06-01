298abde1d77958290cb2c21dd97dfbeefbfd5f26
Sidney Pang <sidneypang@hotmail.co.uk>
2022-06-01 17:31:09 +0000

    finished findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function (object, value) {
+  let keys = Object.keys (object);
+  let values = Object.values (object);
+
+  for (let i in values) {
+    if (values[i] === value) {
+      return keys[i]
+    }
+  }
+
+  return undefined;
+}
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
