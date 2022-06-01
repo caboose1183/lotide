5901c9a40c6513b36f6e8e0a2ae9489634201043
Sidney Pang <sidneypang@hotmail.co.uk>
2022-06-01 17:17:19 +0000

    finished letterPositions.js, made string all lowercase

---
+++ letterPositions.js
@@ -0,0 +1,51 @@
+function eqArrays(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function assertArraysEqual(array1, array2) {
+  if (array1, array2) {
+    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
+  }
+}
+
+
+
+const letterPositions = function (sentence) {
+  const results = {};
+
+  sentence = sentence.toLowerCase();
+
+  for (let i in sentence) {
+    if (sentence[i] !== ' ') {
+      if (results[sentence[i]] === undefined) {
+        results[sentence[i]] = [i];
+      } else {
+        results[sentence[i]].push(i);
+      }
+    }
+  }
+
+
+  return results;
+};
+
+assertArraysEqual(letterPositions("lighthouse in the house").e, { 
+  l: [0],
+  i: [1, 11],
+  g: [2],
+  h: [3, 5, 15, 18],
+  t: [4, 14],
+  o: [6, 19],
+  u: [7, 20],
+  s: [8, 21],
+  e: [9, 16, 22],
+  n: [12]
+}.e);
