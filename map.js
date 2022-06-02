24ec32d45985f3987c77570badb9eb42a5ded86a
Sidney Pang <sidneypang@hotmail.co.uk>
2022-06-02 18:38:00 +0000

    added map.js, tested with assertArraysEqual.js

---
+++ map.js
@@ -0,0 +1,34 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function (array, callback) {
+  const results = [];
+
+  for (let item of array) {
+    results.push(callback(item));
+  }
+
+  return results;
+}
+
+function eqArrays(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function assertArraysEqual(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
+  }
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't'])
