345050edb49b1cb8653bc06b8eac2aeee2550155
Sidney Pang <sidneypang@hotmail.co.uk>
2022-06-06 02:20:41 +0000

    refactored tail, made tailTest.js in test folder, added exports and require

--- head.js
+++ head.js
@@ -1,10 +1,10 @@
-const assertEqual = require ('./assertEqual');
+const assertEqual = require('./assertEqual');
 
 const head = function (array) {
   if (array[0] === '') {
     return undefined;
   } else {
-    return array[0]; 
+    return array[0];
   }
 }
 
--- tail.js
+++ tail.js
@@ -1,13 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-function tail (array) {
-  return array.slice (1);
+function tail(array) {
+  if (array.length === 1 || array[0] === []) {
+    return [];
+  } else {
+    return array.slice(1);
+  }
 }
 
-
+module.exports = tail;
